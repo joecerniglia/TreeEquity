1420a678b2937d5d1f1f55466b402da5ce855c48
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-26 12:44:26 -0400

    Create init.R

---
+++ init.R
@@ -0,0 +1,11 @@
+#init.R
+
+my_packages <- c("shiny", "sf", "sfdep", "spdep", "dplyr", "tidyr", "ggplot2", "rgdal", "terra")
+
+install_if_missing = function(p) {
+  if p %in% rownames(installed.packages()) == FALSE {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
