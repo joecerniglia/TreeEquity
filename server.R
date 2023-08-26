260b72aa2f71ab3ba0637664b182c67f84fc93be
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-26 12:56:30 -0400

    Update server.R

--- server.R
+++ server.R
@@ -3,8 +3,8 @@ server <- function(input, output) {
   
   output$geo <- renderPlot({
     print(input$clustermap)
-    setwd(
-      "/Users/josephcerniglia/Documents/eCornell Data Analytics in R/Hotspots")
+    #setwd(
+    #  "/Users/josephcerniglia/Documents/eCornell Data Analytics in R/Hotspots")
     
     filestring <- tolower(input$select)
     tes_data <-  read.csv(paste0(filestring,"_tes.csv"))
