c1cca307309cb420a45d28405f33e48e68d76f5f
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-26 14:00:17 -0400

    Update init.R

--- init.R
+++ init.R
@@ -1,6 +1,6 @@
 #init.R
 
-my_packages = c("sf", "sfdep", "spdep", "dplyr", "tidyr", "ggplot2")
+my_packages = c("rlang", "sf", "sfdep", "spdep", "dplyr", "tidyr", "ggplot2")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages())) {
