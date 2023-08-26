b7babbbcf9071dded4dbc388b1e8c8d1c3a66345
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-26 13:18:44 -0400

    Update init.R

--- init.R
+++ init.R
@@ -3,9 +3,10 @@
 my_packages = c("sf", "sfdep", "spdep", "dplyr", "tidyr", "ggplot2", "rgdal", "terra")
 
 install_if_missing = function(p) {
-  if (p !%in% rownames(installed.packages())) {
-    install.packages(p)
+  if (p %in% rownames(installed.packages())) {
+     print('Do nothing.')
   }
+  else {install.packages(p)}
 }
 
 invisible(sapply(my_packages, install_if_missing))
