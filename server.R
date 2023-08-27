e2cad0204349498601dd195d2050dd26c26fc393
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-27 08:39:28 -0400

    Update server.R

--- server.R
+++ server.R
@@ -19,11 +19,11 @@ output$hist <- renderPlot({
       #hist_breaks <- hist(tes_data)$breaks
       hist_breaks <- 30
       #define colors to use in histogram based on break points
-      color_list <- rep('darkgreen', length(hist_breaks))
-      color_list[hist_breaks < 85] <- 'green'
-      color_list[hist_breaks < 70] <- 'brown'
+      #color_list <- rep('darkgreen', length(hist_breaks))
+      #color_list[hist_breaks < 85] <- 'green'
+      #color_list[hist_breaks < 70] <- 'brown'
       hist(tes_data, breaks=hist_breaks, main=paste0("Distribution of tree equity scores for ",input$select),
-      xlab="Tree Equity Score [range 0-100]",col=color_list,
+      xlab="Tree Equity Score [range 0-100]",col="darkgreen",
        cex.axis=0.5, font.main=1, cex.main=0.8)})
     }
 
