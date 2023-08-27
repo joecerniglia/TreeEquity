5c51b8f3306715764502a1211a2faa1d883627ae
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-27 09:42:51 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,18 +8,18 @@ ui <- fluidPage(
                               choices = sort(c("AK","AL","CT","FL","MA","VT",
                                                "OR","CA","NY",
                                                "MS","ME","TX" ))),width=4
-                 , 
-            checkboxInput("clustermap", "cluster map", TRUE),
-            verbatimTextOutput("value")
+   #              , 
+   #         checkboxInput("clustermap", "cluster map", TRUE),
+  #          verbatimTextOutput("value")
                 ),
     
     mainPanel(
       fluidPage(
         column(width = 8,
-        plotOutput(outputId = "geo",width = "900px", height="1000px"
-                   
-                   )
-        ),
+    #    plotOutput(outputId = "geo",width = "900px", height="1000px"
+    #               
+    #               )
+    #    ),
         
         column(width = 8,
         plotOutput(outputId="hist",width = "500px", height="500px"
