b49fe92bb3cd684b94264e883da29179923ab04c
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-26 21:06:52 -0400

    Update server.R

--- server.R
+++ server.R
@@ -9,13 +9,16 @@ library(tidyr)
 
 server <- function(input, output) {
   
-  
+color_list <- rep('darkgreen', length(hist_breaks))
+color_list[hist_breaks < 80] <- 'green'
+color_list[hist_breaks < 60] <- 'brown'  
    
 
 output$hist <- renderPlot({
       filestring <- tolower(input$select)
       tes_data <-  read.csv(paste0(filestring,"_tes.csv"))$tesctyscor
-      hist(tes_data,color="darkgreen")
+      hist(tes_data,main=paste0("Distribution of tree equity scores for ",input$select),
+      xlab="Tree Equity Score [range 0-100]",col=color_list)
     })
       
 
