1aaaf4c4ba2e3aeb4924279b13a202af4ccdafd8
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-26 20:33:34 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -7,17 +7,19 @@ ui <- fluidPage(
                               label = "Select a state to see a Tree Map", 
                               choices = sort(c("AK","AL","CT","FL","MA","VT",
                                                "OR","CA","NY",
-                                               "MS","ME","TX" ))),width=1, 
-            checkboxInput("clustermap", "cluster map", TRUE),
-            verbatimTextOutput("value")),
+                                               "MS","ME","TX" ))),width=1
+                 #, 
+            #checkboxInput("clustermap", "cluster map", TRUE),
+            #verbatimTextOutput("value")
+                ),
     
     mainPanel(
       fluidPage(
-        column(width = 8,
-        plotOutput(outputId = "geo",width = "900px", height="1000px"
+        #column(width = 8,
+        #plotOutput(outputId = "geo",width = "900px", height="1000px"
                    
-                   )
-        ),
+                  # )
+        #),
         
         column(width = 3,
         plotOutput(outputId="hist",width = "280px", height="250px"
