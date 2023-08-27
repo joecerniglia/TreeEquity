5a0d79c1446d413393e9000e1f453b5c7507cbe5
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-27 08:31:09 -0400

    Update server.R

--- server.R
+++ server.R
@@ -16,12 +16,13 @@ output$hist <- renderPlot({
       filestring <- tolower(input$select)
       tes_data <-  read.csv(paste0(filestring,"_tes.csv"))$tesctyscor
       #define histogram break points
-      hist_breaks <- hist(tes_data)$breaks
+      #hist_breaks <- hist(tes_data)$breaks
+      hist_breaks <- 30
       #define colors to use in histogram based on break points
       color_list <- rep('darkgreen', length(hist_breaks))
       color_list[hist_breaks < 85] <- 'green'
       color_list[hist_breaks < 70] <- 'brown'
-      hist(tes_data, breaks=30, main=paste0("Distribution of tree equity scores for ",input$select),
+      hist(tes_data, breaks=hist_breaks, main=paste0("Distribution of tree equity scores for ",input$select),
       xlab="Tree Equity Score [range 0-100]",col=color_list,
        cex.axis=0.5, font.main=1, cex.main=0.8)})
     }
