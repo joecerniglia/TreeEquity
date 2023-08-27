f9aee8f55cb4c7727c12e0cc86dffbe4defba31b
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-26 22:08:38 -0400

    Update server.R

--- server.R
+++ server.R
@@ -17,7 +17,7 @@ output$hist <- renderPlot({
       tes_data <-  read.csv(paste0(filestring,"_tes.csv"))$tesctyscor
       hist(tes_data, breaks="Sturges", main=paste0("Distribution of tree equity scores for ",input$select),
       xlab="Tree Equity Score [range 0-100]",col="darkgreen")}
-    })
+    )}
 
 
       
