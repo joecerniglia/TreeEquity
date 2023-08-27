0d441e89ead9893fb06433297384a48d810aa76c
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-26 21:40:04 -0400

    Update server.R

--- server.R
+++ server.R
@@ -8,23 +8,24 @@ library(tidyr)
 
 
 server <- function(input, output) {
-filestring <- tolower(input$select)
-#define histogram break points
-hist_breaks <- hist(read.csv(paste0(filestring,"_tes.csv"))$tesctyscor)$breaks
-  
-color_list <- rep('darkgreen', length(hist_breaks))
-color_list[hist_breaks < 80] <- 'green'
-color_list[hist_breaks < 60] <- 'brown'  
-   
 
-output$hist <- renderPlot({
-      #filestring <- tolower(input$select)
+  
+
+
+output$hist <- renderPlot(
+      filestring <- tolower(input$select)
+      #define histogram break points
+      hist_breaks <- hist(read.csv(paste0(filestring,"_tes.csv"))$tesctyscor)$breaks
+      color_list <- rep('darkgreen', length(hist_breaks))
+      color_list[hist_breaks < 80] <- 'green'
+      color_list[hist_breaks < 60] <- 'brown'  
+   
       tes_data <-  read.csv(paste0(filestring,"_tes.csv"))$tesctyscor
       hist(tes_data,breaks="Sturges", main=paste0(
       "Distribution of tree equity scores for ",input$select),
-      xlab="Tree Equity Score [range 0-100]",col=color_list)
-    })
+      xlab="Tree Equity Score [range 0-100]",col=color_list))
+    }
       
 
   
-}
+
