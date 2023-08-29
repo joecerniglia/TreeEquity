343c066cc24ea5f126f48aed610d506b05659ff1
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-29 05:34:40 -0400

    Add files via upload

---
+++ app.R
@@ -0,0 +1,285 @@
+library(shiny)
+library(ggplot2)
+library(sf)
+library(sfdep)
+library(spdep)
+library(dplyr)
+library(tidyr)
+library(rgdal)
+library(terra)
+library(shinydashboard)
+
+options(scipen=999)
+abbrev<-c("AK", "AL", "AR", "AZ", "CA", 
+          "CO", "CT", "DC", "DE", "FL", 
+          "GA", "HI", "IA", "ID", "IL", 
+          "IN", "KS", "KY", "LA", "MA", 
+          "MD", "ME", "MI", "MN", "MO", 
+          "MS", "MT", "NC", "ND", "NE", 
+          "NH", "NJ", "NM", "NV", "NY", 
+          "OH", "OK", "OR", "PA", "RI", 
+          "SC", "SD", "TN", "TX", "UT", 
+          "VA", "VT", "WA", "WI", "WV", 
+          "WY")
+states<-c("Alaska", "Alabama", "Arkansas", "Arizona",  
+          "California", "Colorado", "Connecticut", "District of Columbia",
+          "Delaware", "Florida", "Georgia", 
+          "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", 
+          "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", 
+          "Michigan",  "Minnesota","Missouri", 
+          "Mississippi", "Montana", "North Carolina",  "North Dakota", 
+          "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", 
+          "New York", "Ohio", "Oklahoma", "Oregon",   
+          "Pennsylvania", "Rhode Island", 
+          "South Carolina", "South Dakota", "Tennessee", "Texas", 
+          "Utah", "Virginia",
+          "Vermont",  "Washington", "Wisconsin","West Virginia", "Wyoming")
+state_lookup <- cbind(abbrev,states)
+
+ui <- fluidPage(
+  titlePanel("Tree Equity in the United States"),
+  
+
+  sidebarLayout(
+    
+    sidebarPanel(selectInput( inputId = "select", 
+                              label = "Select a state to see a tree map:", 
+                              choices = list('Alabama' = 'AL',
+                                            'Alaska'='AK','Arizona'='AZ',
+                                             'Arkansas'='AR',
+'California'='CA','Colorado'='CO','Connecticut'='CT','Delaware'='DE',
+'District of Columbia'='DC','Florida'='FL','Georgia'='GA','Hawaii'='HI',
+'Idaho'='ID','Illinois'='IL','Indiana'='IN','Iowa'='IA','Kansas'='KS',
+'Kentucky'='KY','Louisiana'='LA','Maine'='ME',
+'Maryland'='MD','Massachusetts'='MA',
+'Michigan'='MI','Minnesota'='MN','Mississippi'='MS','Missouri'='MS',
+'Montana'='MT','Nebraska'='NE','Nevada'='NV','New Hampshire'='NH',
+'New Jersey'='NJ', 'New Mexico'='NM',
+'New York'='NY',
+'North Carolina'='NC','North Dakota'='ND',
+'Ohio'='OH','Oklahoma'='OK','Oregon'='OR',
+'Pennsylvania'='PA','Rhode Island'='RI','South Carolina'='SC',
+'South Dakota'='SD','Tennessee'='TN','Texas'='TX','Utah'='UT','Vermont'='VT',
+'Virginia'='VA',
+'Washington'='WA','West Virginia'='WV','Wisconsin'='WI','Wyoming'='WY')), 
+                 width="2", 
+            checkboxInput("clustermap", "cluster map", FALSE),
+            verbatimTextOutput("value"),
+            checkboxInput("colorblind", "color-blind", FALSE),
+            verbatimTextOutput("value2")),
+    
+    mainPanel(
+      fluidPage(
+        column(width = 7,
+        plotOutput(outputId = "geo",width = "700px", height="700px"
+                   
+                   )
+        ),
+        
+        column(width = 3,
+        plotOutput(outputId="hist",width = "280px", height="250px"
+                   )),
+  
+
+tags$footer(
+  "Data Source: ",
+  tags$a(
+    "treeequityscore.org/methodology.",
+    target = "_blank",
+    href = "https://www.treeequityscore.org/methodology"),
+   " Template for statistical source code: ",
+  tags$a(
+    "https://rpubs.com/heatherleeleary/hotspot_getisOrd_tut.",
+    target = "_blank",
+    href = "https://rpubs.com/heatherleeleary/hotspot_getisOrd_tut"),
+    style = "position:absolute;bottom:0; width: 100%; color: black; text-align: center;"
+)))))  
+
+
+
+server <- function(input, output) {
+  
+  
+  output$geo <- renderPlot({
+    print(input$clustermap)
+    
+    #comment out the set working directory function to deploy to shinyapps.io
+    #setwd(
+    # "/Users/josephcerniglia/Documents/eCornell Data Analytics in R/Hotspots/App-4")
+    #green red palette is #8
+    #Palette #7 will turn the green to blue for a greater
+    #accessibility to the color-blind.
+    if (input$colorblind==TRUE) {Pale<-7} else {Pale<-8}
+    if (input$colorblind==TRUE) {Gradi<-"#0000FF"} else {Gradi<-"#013220"}
+    filestring <- tolower(input$select)
+    tes_data <-  read.csv(paste0(filestring,"_tes.csv"))
+    tes_data_g <- st_read(paste0(filestring,"_tes.shp"))
+    tes_data <- cbind(tes_data, geometry = tes_data_g$geometry)
+    row_count <- nrow(tes_data)
+    boundary_factor <- (1/row_count)*100
+    print(row_count)
+  
+    tes_data <- tes_data[!is.na(tes_data$tesctyscor),]
+    # Create a neighbor list based on queen contiguity
+    list_nb <- poly2nb(tes_data$geometry, queen = TRUE)
+    # Check for empty neighbor sets
+    # card() calculates number of neighbors for each polygon in the list
+    # which() finds polygons with 0 neighbors
+    empty_nb <- which(card(list_nb) == 0)
+    #empty_nb  
+    # Remove polygons with empty neighbor sets from the data
+    if (length(empty_nb)>0) {
+      print('Empty neighbor sets found')
+      tes_subset <- tes_data[-empty_nb, ]
+    } else {
+      print('No empty neighbor sets found')
+      tes_subset <- tes_data
+    }
+    
+    # Now that we removed empty neighbor sets, (tes_subset).
+    # once again identify neighbors with queen contiguity (edge/vertex touching)
+    #View(tes_subset)
+    tes_nb <- poly2nb(tes_subset$geometry, queen=TRUE)
+    #View(tes_nb)
+    # Binary weighting assigns a weight of 1 to all neighboring features 
+    # and a weight of 0 to all other features
+    tes_w_binary <- nb2listw(tes_nb, style="B")
+    ##tes_w_binary_g <- nb2listw(tes_nb_g, style="B")
+    
+    # Calculate spatial lag of tree equity score
+    tes_lag <- lag.listw(tes_w_binary, tes_subset$tesctyscor)
+    
+    #globalG.test (or global_g_test()) computes a global test for spatial 
+    #autocorrelation using a Monte Carlo simulation approach (simulated spatial 
+    #datasets that have the same spatial structure as the original data but 
+    #are randomly permuted). It tests the null hypothesis of no spatial 
+    #autocorrelation against the alternative hypothesis of positive spatial 
+    #autocorrelation.
+    #The output includes a p-value.  Is it significant?
+    p_value <- globalG.test(tes_subset$tesctyscor, tes_w_binary)$p[1,]
+    p2 <- if(p_value<=.001){"[global p < 0.001]"}
+    else {paste0("[global p=",round(p_value,3),"]")}
+    #print(p2)
+    
+    
+    if (input$clustermap==TRUE) {
+        #Identify neighbors, create weights, calculate spatial lag
+        #Definition of spatial lag:
+        #a weighted sum or a weighted average of the neighboring values for the 
+        #variable of interest
+        tes_nbs <- tes_subset |> 
+          mutate(
+            nb = st_contiguity(geometry),       # neighbors share border/vertex
+            wt = st_weights(nb),                 # row-standardized weights
+            tes_lag = st_lag(tesctyscor, nb, wt) # calculate spatial lag of 
+                                                # tesctyscor (tree equity score)
+          ) 
+        
+        
+        
+        
+        # Calculate the Gi using local_g_perm, as tes_hot_spots.
+        #The Gi is the ratio of the spatial lag of a feature to the sum of the 
+        #feature’s values for its neighbors. A positive Gi value indicates that 
+        #a feature and its neighbors have high values, while a negative Gi value 
+        #indicates that they have low values. The magnitude of the Gi value 
+        #indicates the strength of the clustering.
+        
+        tes_hot_spots <- tes_nbs |> 
+          mutate(
+            Gi = local_g_perm(tesctyscor, nb, wt, nsim = 999)
+            # nsim = number of Monte Carlo simulations (999 is default)
+          ) |> 
+          # The new 'Gi' column itself contains a dataframe 
+          # We can't work with that, so we need to 'unnest' it
+          unnest(Gi) 
+        
+        
+            # final graph with piped input of tes_hot_spots
+            tes_hot_spots |> 
+              # with the columns 'gi' and 'p_folded_sim"
+              # 'p_folded_sim' is the p-value of a folded permutation test
+              select(gi, p_folded_sim, geometry) |> 
+              mutate(
+                # Add a new column called "classification"
+                classification = case_when(
+                  # Classify based on the following criteria:
+                  gi > 0 & p_folded_sim <= 0.01 ~ "Very green",
+                  gi > 0 & p_folded_sim <= 0.05 ~ "Moderately green",
+                  gi > 0 & p_folded_sim <= 0.1 ~ "Somewhat green",
+                  gi < 0 & p_folded_sim <= 0.01 ~ "Very bare",
+                  gi < 0 & p_folded_sim <= 0.05 ~ "Moderately bare",
+                  gi < 0 & p_folded_sim <= 0.1 ~ "Somewhat bare",
+                  TRUE ~ "No cluster detected"
+                ),
+                # Convert 'classification' into a factor for easier plotting
+                classification = factor(
+                  classification,
+                  levels = c("Very green", "Moderately green", "Somewhat green",
+                             "No cluster detected",
+                             "Somewhat bare", "Moderately bare", "Very bare")
+                )
+              ) |> 
+              # Visualize the results with ggplot2
+              #Pale<-7
+              ggplot(aes(fill = classification)) +
+              geom_sf(aes(geometry=geometry),color = "black", 
+                      lwd = boundary_factor) +
+              coord_sf(expand = TRUE) +
+              scale_fill_brewer(type = "div", palette = Pale, direction=-1) +
+      
+              theme_void(
+                base_size = 18,
+                base_family = "",
+                base_line_size = base_size/22,
+                base_rect_size = base_size/22
+              ) +
+              theme(
+                    plot.title = element_text(hjust = 0.5),
+                    legend.position = "right") +
+              theme_void(
+                base_size = 18,
+                base_family = "",
+                base_line_size = base_size/20,
+                base_rect_size = base_size/20
+              ) +
+              labs(
+                fill = "Green Spot \n Classification",
+                title = paste("Tree Equity Green Spots in", 
+                              states[which(state_lookup == input$select)],
+                              "\n",p2))
+            }
+    else {ggplot(tes_subset) +
+        geom_sf(aes(geometry=geometry,fill = tesctyscor), color = "black", 
+                lwd = boundary_factor) +
+       
+      scale_fill_gradient(
+          name = "Tree Equity Score",
+                            low = "red",
+                            high = Gradi) +
+       
+        theme(
+          plot.title = element_text(hjust = 0.5),
+          legend.position = "right") +
+        theme_void(base_size = 18,
+                   base_family = "",
+                   base_line_size = base_size/22,
+                   base_rect_size = base_size/22) +
+        labs(title = paste(
+        "Tree Equity Scores of",
+        states[which(state_lookup == input$select)],"\nNeighborhoods"))
+        }
+  })
+    output$hist <- renderPlot({
+      filestring <- tolower(input$select)
+      tes_data <-  read.csv(paste0(filestring,"_tes.csv"))$tesctyscor
+      hist(tes_data, main=paste0(
+        "Tree equity scores for ",input$select),
+        xlab=paste0("Tree Equity Score [range 0-100]"))
+    })
+      
+
+    
+}
+shinyApp(ui, server)
+
