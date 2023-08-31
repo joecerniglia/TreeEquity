7bca89ae0a874b4bcb2a0f348b4715503eac9e81
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2023-08-30 20:48:50 -0400

    Update app.R

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ library(tidyr)
 library(rgdal)
 library(terra)
 library(shinydashboard)
+library(shinythemes)
 
 options(scipen=999)
 abbrev<-c("AK", "AL", "AR", "AZ", "CA", 
@@ -63,9 +64,9 @@ ui <- fluidPage(
 'Virginia'='VA',
 'Washington'='WA','West Virginia'='WV','Wisconsin'='WI','Wyoming'='WY')), 
                  width="2", 
-            checkboxInput("clustermap", "cluster map", FALSE),
+            checkboxInput("clustermap", "Cluster Map", FALSE),
             verbatimTextOutput("value"),
-            checkboxInput("colorblind", "color-blind", FALSE),
+            checkboxInput("colorblind", "Color Enhancement (for better contrast or to assist the color-blind)", FALSE),
             verbatimTextOutput("value2")),
     
     mainPanel(
@@ -87,7 +88,7 @@ tags$footer(
     "treeequityscore.org/methodology.",
     target = "_blank",
     href = "https://www.treeequityscore.org/methodology"),
-   " Template for statistical source code: ",
+   "  Spatial autocorellation template: ",
   tags$a(
     "https://rpubs.com/heatherleeleary/hotspot_getisOrd_tut.",
     target = "_blank",
@@ -102,15 +103,29 @@ server <- function(input, output) {
   
   output$geo <- renderPlot({
     print(input$clustermap)
-    
+  	 # Create a Progress object
+   	progress <- shiny::Progress$new()
+  	 # Make sure it closes when we exit this reactive, even if there's an error
+  	 on.exit(progress$close())
+  	 progress$set(message = "Building tree equity map...", value = 0)
     #comment out the set working directory function to deploy to shinyapps.io
     #setwd(
     # "/Users/josephcerniglia/Documents/eCornell Data Analytics in R/Hotspots/App-4")
     #green red palette is #8
     #Palette #7 will turn the green to blue for a greater
     #accessibility to the color-blind.
+  	 #90EE90=light green ;  #013220=dark green. #0000FF=dark blue
+  	 #800000=maroon #B66B3E=tan #8E4B32=chestnut #ADD8E6=light blue (cyan)
+  	 #print("#3D0C02") #=dark bean
+  	 #F5F5F5=light grey ;  ##57504d=dark grey
     if (input$colorblind==TRUE) {Pale<-7} else {Pale<-8}
-    if (input$colorblind==TRUE) {Gradi<-"#0000FF"} else {Gradi<-"#013220"}
+    if (input$colorblind==TRUE) {Gradi1<-"#800000" 
+    Gradi2<-"#ADD8E6"}
+  	 else 
+  	 {Gradi1<-"#5C2C26" 
+    	Gradi2<-"#90EE90"}
+  	 n<-7
+  	 progress$inc(1/n, detail = paste("Reading in data: step", 1))
     filestring <- tolower(input$select)
     tes_data <-  read.csv(paste0(filestring,"_tes.csv"))
     tes_data_g <- st_read(paste0(filestring,"_tes.shp"))
@@ -120,50 +135,62 @@ server <- function(input, output) {
     print(row_count)
   
     tes_data <- tes_data[!is.na(tes_data$tesctyscor),]
-    # Create a neighbor list based on queen contiguity
-    list_nb <- poly2nb(tes_data$geometry, queen = TRUE)
-    # Check for empty neighbor sets
-    # card() calculates number of neighbors for each polygon in the list
-    # which() finds polygons with 0 neighbors
-    empty_nb <- which(card(list_nb) == 0)
-    #empty_nb  
-    # Remove polygons with empty neighbor sets from the data
-    if (length(empty_nb)>0) {
-      print('Empty neighbor sets found')
-      tes_subset <- tes_data[-empty_nb, ]
-    } else {
-      print('No empty neighbor sets found')
-      tes_subset <- tes_data
-    }
-    
-    # Now that we removed empty neighbor sets, (tes_subset).
-    # once again identify neighbors with queen contiguity (edge/vertex touching)
-    #View(tes_subset)
-    tes_nb <- poly2nb(tes_subset$geometry, queen=TRUE)
-    #View(tes_nb)
-    # Binary weighting assigns a weight of 1 to all neighboring features 
-    # and a weight of 0 to all other features
-    tes_w_binary <- nb2listw(tes_nb, style="B")
-    ##tes_w_binary_g <- nb2listw(tes_nb_g, style="B")
-    
-    # Calculate spatial lag of tree equity score
-    tes_lag <- lag.listw(tes_w_binary, tes_subset$tesctyscor)
-    
-    #globalG.test (or global_g_test()) computes a global test for spatial 
-    #autocorrelation using a Monte Carlo simulation approach (simulated spatial 
-    #datasets that have the same spatial structure as the original data but 
-    #are randomly permuted). It tests the null hypothesis of no spatial 
-    #autocorrelation against the alternative hypothesis of positive spatial 
-    #autocorrelation.
-    #The output includes a p-value.  Is it significant?
-    p_value <- globalG.test(tes_subset$tesctyscor, tes_w_binary)$p[1,]
-    p2 <- if(p_value<=.001){"[global p < 0.001]"}
-    else {paste0("[global p=",round(p_value,3),"]")}
-    #print(p2)
-    
     
     if (input$clustermap==TRUE) {
-        #Identify neighbors, create weights, calculate spatial lag
+				    #if (input$select=='CA') {tes_data <- sample_n(tes_data,10000)}
+				    # Create a neighbor list based on queen contiguity
+				    progress$inc(1/n, detail = paste("Removing polygons with empty neighbor sets from the data: step", 2))
+				    list_nb <- poly2nb(tes_data$geometry, queen = TRUE)
+				    # Check for empty neighbor sets
+				    # card() calculates number of neighbors for each polygon in the list
+				    # which() finds polygons with 0 neighbors
+				    empty_nb <- which(card(list_nb) == 0)
+				    #empty_nb  
+				    # Remove polygons with empty neighbor sets from the data
+				    if (length(empty_nb)>0) {
+				      print('Empty neighbor sets found')
+				      tes_subset <- tes_data[-empty_nb, ]
+				    } else {
+				      print('No empty neighbor sets found')
+				      tes_subset <- tes_data
+				    }
+    
+    
+    
+				    # Now that we removed empty neighbor sets, (tes_subset).
+				    # once again identify neighbors with queen contiguity (edge/vertex touching)
+				    #View(tes_subset)
+				    tes_nb <- poly2nb(tes_subset$geometry, queen=TRUE)
+				    #View(tes_nb)
+				    # Binary weighting assigns a weight of 1 to all neighboring features 
+				    # and a weight of 0 to all other features
+				    tes_w_binary <- nb2listw(tes_nb, style="B")
+				    ##tes_w_binary_g <- nb2listw(tes_nb_g, style="B")
+				    
+				    # Calculate spatial lag of tree equity score
+				    tes_lag <- lag.listw(tes_w_binary, tes_subset$tesctyscor)
+				    progress$inc(1/n, detail = paste("Calculating global p value: step", 3))
+				    #globalG.test (or global_g_test()) computes a global test for spatial 
+				    #autocorrelation using a Monte Carlo simulation approach (simulated spatial 
+				    #datasets that have the same spatial structure as the original data but 
+				    #are randomly permuted). It tests the null hypothesis of no spatial 
+				    #autocorrelation against the alternative hypothesis of positive spatial 
+				    #autocorrelation.
+				    #The output includes a p-value.  Is it significant?
+				    p_value <- globalG.test(tes_subset$tesctyscor, tes_w_binary)$p[1,]
+				    p2 <- if(p_value<=.001 & input$select != "DC")
+				      {"[global p < 0.001]"}
+				    else if(input$select != "DC") {paste0(
+				       "[global p=",round(p_value,3),"]")}
+				    else {"Insufficient Data"}
+				    #print(p2)
+    
+
+    				progress$inc(1/n, detail = paste(
+    				"Calculating spatial lag, a weighted average of the neighboring tree equity scores: step", 4))
+    				
+    				
+        #Identify neighbors, create weights, calculate spatial lag.
         #Definition of spatial lag:
         #a weighted sum or a weighted average of the neighboring values for the 
         #variable of interest
@@ -175,8 +202,11 @@ server <- function(input, output) {
                                                 # tesctyscor (tree equity score)
           ) 
         
-        
-        
+     
+        # Increment the progress bar, and update the detail text.
+      
+        progress$inc(1/n, detail = paste(
+        	"Calculating Gi, indicating cluster strength, and local p's with a Monte Carlo simulation: step", 5))
         
         # Calculate the Gi using local_g_perm, as tes_hot_spots.
         #The Gi is the ratio of the spatial lag of a feature to the sum of the 
@@ -194,7 +224,7 @@ server <- function(input, output) {
           # We can't work with that, so we need to 'unnest' it
           unnest(Gi) 
         
-        
+            progress$inc(1/n, detail = paste("Plotting final map:", 6)) 
             # final graph with piped input of tes_hot_spots
             tes_hot_spots |> 
               # with the columns 'gi' and 'p_folded_sim"
@@ -249,14 +279,14 @@ server <- function(input, output) {
                               states[which(state_lookup == input$select)],
                               "\n",p2))
             }
-    else {ggplot(tes_subset) +
+    else {ggplot(tes_data) +
         geom_sf(aes(geometry=geometry,fill = tesctyscor), color = "black", 
                 lwd = boundary_factor) +
        
       scale_fill_gradient(
           name = "Tree Equity Score",
-                            low = "red",
-                            high = Gradi) +
+                            low =  Gradi1,
+                            high = Gradi2) +
        
         theme(
           plot.title = element_text(hjust = 0.5),
